425d6a24a94e41372d3fa60b3ac88cf82d2785a3
Julian Wilson <julian@nextgenbt.com>
2019-04-01 16:20:08 -0400

    no 0x

--- app/app.js
+++ app/app.js
@@ -148,10 +148,9 @@ export default class App {
       console.log(`Signing hash of order`);
       console.log(makerOrder);
       const orderHash = getHash(makerOrder);
-      const orderHashNo0x = orderHash.substring(2)
       console.log(`Bitski hash to sign: ${orderHash}`);
       bitskiSigner.provider
-        .send("eth_sign", [address.toLowerCase(), orderHashNo0x])
+        .send("eth_sign", [address.toLowerCase(), orderHash])
         .then(signature => {
           console.log(`Bitski signature: ${signature}`);
           this.myContractEthersWrapper
